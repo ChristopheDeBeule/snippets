9fe16cdc759f414e3c488e96e2ead6d1976bba32
christophe.debeule <christophe.debeule@idalko.com>
2024-02-09 14:24:57 -0800

    FIX: Parent field Update
    Jira Cloud updated their "Epic name" field to Parent.
    I applied changes that the script keeps working after this update.
    NEW function: setNewParentField(localIssue: localIssue)

--- ADO <> Jira Cloud Keep Epic -> Story -> Sub-task hierarchy/Jira Cloud Incoming script.groovy
+++ ADO <> Jira Cloud Keep Epic -> Story -> Sub-task hierarchy/Jira Cloud Incoming script.groovy
@@ -1,26 +1,37 @@
+// This will set the new Parent field in Jira Cloud
+def setNewParentField(def localIssue){
+   def url = "/rest/api/3/issue/${issue.key}/"
+   def dataKey = null
+   // We'll check if the remote side has an Epic Link, if they removed their epic link it will also be removed on the local side.
+   // If we want to keep the epic link even when removed on the remote site remove the null check and the dataKey var.
+   if(localIssue != null) dataKey = "\"${localIssue}\""
+
+   def data = "{\"fields\": {\"parent\": {\"key\" : ${dataKey}}}}"
+   
+   httpClient.put(url,data)
+}
+
 if(firstSync){
    issue.projectKey   = "DEMO" 
    // Set type name from source issue, if not found set a default
    def typeMap = [
       // "ADO side":"Jira Cloud side"
-      "Feature":"Epic",
-      "Epic":"Feature",
+      "Epic":"Epic",
+      "Feature":"Feature",
       "User Story":"Story"
    ]
    issue.typeName     = nodeHelper.getIssueType(typeMap[replica.type?.name], issue.projectKey)?.name ?: nodeHelper.getIssueType(replica.type?.name, (issue.projectKey ?: issue.project.key))?.name ?: "Task"
    issue.summary      = replica.summary
-   if (replica.typeName == "Feature") {
-      issue.customFields."Epic Name".value = replica.summary
-   }
    store(issue)
+
 }
 // This will check if the issueType is a Feature and if there is a parentId now it will link the epic with the feature.
 // And it will only link the issues as a child issue under the feature
+// NOTE: Change the issueType to the type that needs the parent link.
 if (issue.typeName == "Feature" && replica.parentId) {
-    def localParent = syncHelper.getLocalIssueKeyFromRemoteId(replica.parentId.toLong())
-    if (localParent) {
-        issue.customFields."Epic Link".value = localParent.urn
-    }
+   def localParent = syncHelper.getLocalIssueKeyFromRemoteId(replica.parentId.toLong())
+   setNewParentField(localParent.urn)
+   
 }else {
 
    replica.relations.each {   
