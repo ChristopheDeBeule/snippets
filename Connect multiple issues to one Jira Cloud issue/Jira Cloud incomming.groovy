7289440c47952cf39683f56886e0910bb6326381
christophe.debeule <christophe.debeule@idalko.com>
2024-03-15 16:30:39 +0100

    UPDATE: Check if the remoteUrn field is not empty
    Added the functionallity to check if the string of the remoteUrn is not empty.
    We do this so we don't do a replace on a null object and prevent errors

--- Connect multiple issues to one Jira Cloud issue/Jira Cloud incomming.groovy
+++ Connect multiple issues to one Jira Cloud issue/Jira Cloud incomming.groovy
@@ -1,6 +1,9 @@
 def remoteIssueUrn = replica.customFields."CF Name"?.value 
+// This will check if there is a remote issue URN so we can do a replace otherwise you do a replace on a null object which can cause errors.
+if (remoteIssueUrn){
+    remoteIssueUrn = remoteIssueUrn.replaceAll("\\s","")
+}
 // removes whitespaces in string 
-remoteIssueUrn = remoteIssueUrn.replaceAll("\\s","")
 if(remoteIssueUrn && firstSync){
     def localIssue = httpClient.get("/rest/api/2/issue/"+remoteIssueUrn)
     if(localIssue == null) throw new com.exalate.api.exception.IssueTrackerException("Issue with key "+remoteIssueUrn+" was not found")
@@ -11,6 +14,7 @@ if(remoteIssueUrn && firstSync){
     def getCustomFieldValue = httpClient.get("/rest/api/3/issue/${issue.key}")?.fields?.customfield_10035 // String NOTE: customfield_10035 is the custom field where you want to set the counter in (This is a text field)
     def foundNumber = 0
     if (getCustomFieldValue){
+        // This will check if there is a number in your string and use that number
         foundNumber = (getCustomFieldValue =~ /\d+/)[0]
     }
     issue.customFields."Demo".value = "Connected Tickets: ${(foundNumber.toInteger() + 1).toString()}"
