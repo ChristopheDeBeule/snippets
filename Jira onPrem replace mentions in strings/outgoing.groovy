3702eee7a477d4d22856176ff19334acda662908
christophe.debeule <christophe.debeule@idalko.com>
2024-03-26 23:40:38 +0100

    NEW: Remove mentions from Jira description or comments.
    This code snippet will remove mentions from a jira comment or description.

---
+++ Jira onPrem replace mentions in strings/outgoing.groovy
@@ -0,0 +1,32 @@
+// This wil replace mentions in a string
+// mentions: [~ jiraUserName]
+
+def replaceMentions(String str) 
+    // This patern will search for all the [~mentions]
+    def pattern = /\[\~[^\]]+\]/
+    // Find all matches in the string
+    def matches = (str =~ pattern)
+    // Use this patern to find the username
+    def replacePattern = /\[~(.+)\]/ 
+    def description = issue.description
+
+    // iterate over every match it found
+    matches.each { c ->
+        // use this matcher to find the display name
+        matcher = (c =~ replacePattern)
+        username = matcher[0][1]
+        // If we have a display name we replace it
+        if (username){
+            target = nodeHelper.getUserByUsername(username)?.displayName
+            str = str.replace(c, target)
+        }
+    }
+    return str
+}
+
+replica.description = replaceMentions(issue.description)
+
+replica.comments = issue.comments.collect { comment ->
+    comment.body = replaceMentions(comment.body)
+    comment
+}
