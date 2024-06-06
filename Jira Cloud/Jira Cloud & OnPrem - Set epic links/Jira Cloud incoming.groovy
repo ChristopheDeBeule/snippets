a6844c2625d8e10f3279f2e9be8c3dfdc1e67b3b
christophe.debeule <christophe.debeule@idalko.com>
2024-06-06 16:40:18 -0700

    REFACTOR: Created a new way to set the new parent field in JC

--- Jira Cloud/Jira Cloud & OnPrem - Set epic links/Jira Cloud incoming.groovy
+++ Jira Cloud/Jira Cloud & OnPrem - Set epic links/Jira Cloud incoming.groovy
@@ -1,16 +1,27 @@
-def setNewParentField(){
-   // This will set the new Parent field in Jira Cloud
-    def remoteEpic = replica.customFields."Epic Link"?.value?.urn
-    def localIssue = nodeHelper.getLocalIssueFromRemoteUrn(remoteEpic)?.key
-    def url = "/rest/api/3/issue/${issue.key}/"
-    def dataKey = null
-    // We'll check if the remote side has a Epic Link, if they removed their epic link it will also be removed on the local side.
-    // If we want to keep the epic link even when removed on the remote site remove the null check and the dataKey var.
-    if(remoteEpic != null) dataKey = "\"${localIssue}\""
+// OLD way
+// def setNewParentField(){
+//    // This will set the new Parent field in Jira Cloud
+//     def remoteEpic = replica.customFields."Epic Link"?.value?.urn
+//     def localIssue = nodeHelper.getLocalIssueFromRemoteUrn(remoteEpic)?.key
+//     def url = "/rest/api/3/issue/${issue.key}/"
+//     def dataKey = null
+//     // We'll check if the remote side has a Epic Link, if they removed their epic link it will also be removed on the local side.
+//     // If we want to keep the epic link even when removed on the remote site remove the null check and the dataKey var.
+//     if(remoteEpic != null) dataKey = "\"${localIssue}\""
 
-    def data = "{\"fields\": {\"parent\": {\"key\" : ${dataKey}}}}"
+//     def data = "{\"fields\": {\"parent\": {\"key\" : ${dataKey}}}}"
     
-    httpClient.put(url,data)
+//     httpClient.put(url,data)
+// }
+
+// NEW Way
+def setNewParentField(remoteIssue){
+    // Make sure the issue Type is not a sub-task and that we have a parentId
+  if(!replica.parentId || replica.type?.name == "sub-task") return
+  def localIssue = nodeHelper.getLocalIssueFromRemoteId(replica.parentId.toLong())
+  if(localIssue){
+    issue.parentId = localIssue.id
+  }
 }
 
 if(firstSync){
--- Jira Cloud/Jira Cloud & OnPrem - Set epic links/Jira cloud outgoing.groovy
+++ Jira Cloud/Jira Cloud & OnPrem - Set epic links/Jira cloud outgoing.groovy
@@ -1,2 +1,9 @@
-def parentLink = httpClient.get("/rest/api/3/issue/${issue.key}").fields?.parent?.key
-replica."Epic Link" = parentLink
+// Old Way
+// def parentLink = httpClient.get("/rest/api/3/issue/${issue.key}").fields?.parent?.key
+// replica."Epic Link" = parentLink
+
+
+
+// New Way
+
+replica.parentId = issue.parentId
