6629b0c6e2d40cc7b9b414bb8019f8c9177c58a1
christophe.debeule <christophe.debeule@idalko.com>
2024-06-14 13:43:42 -0700

    REFACTOR: Changes the script to be more performant

--- Add remote URL link as the first comment/incoming.groovy
+++ Add remote URL link as the first comment/incoming.groovy
@@ -1,7 +1,7 @@
 // This will add the remote issue URL as the first comment if there are no comments yet or if the url is not yet in the comments
 
 if(issue.comments.isEmpty() || !issue.comments.body.contains(issueUrl)){
-  issue.comments +=  commentHelper.addComment(issueUrl, issue.comments)
+  issue.comments +=  commentHelper.addComment(issueUrl, false, issue.comments)// .collect{it.internal = false; it} // This will make sure the comment is public
 }
 
 // This can be different depending which connector you're using 
--- Add remote URL link as the first comment/outgoing.groovy
+++ Add remote URL link as the first comment/outgoing.groovy
@@ -1,7 +1,2 @@
-// This will filter out the Jira (atlassian) comments so the Jira issue url will not be added in the remote comments
-
-
-// NOTE: this will check the url so we check first does it start with http and then does it contain the local domain (the domain is easy to look for in a url)
-// Change the domain ("atlassian") to your local domain -> Zendesk, serviceNow, Azure DevOps, Salesfroce, GitHub... (Jira on Prem use your own domain)
-replica.comments = issue.comments.collect{ it.body.startsWith("http") && it.body.contains("atlassian") ? null : it }.findAll { it != null }
+replica.comments = issue.comments
 
