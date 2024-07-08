2ae90fc33f6bbb5da1cd3d921a315bf7515b7e00
christophe.debeule <christophe.debeule@idalko.com>
2024-07-08 10:10:04 -0700

    FIX: Comments will be added on the first sync and we have now 2 connectors
    Jira cloud & Azure DevOps compatible

--- Salesforce/Add User Mentions in Comments/incoming.groovy
+++ Salesforce/Add User Mentions in Comments/incoming.groovy
@@ -1,13 +1,16 @@
 import groovy.json.JsonOutput
 
+if(firstSync){
+  entity.entityType = "Case"
+}
 
-def setMessageSegment(String type){
+def getMessageSegment(String type){
   def mentionMatch = type =~ /\[~accountid:(.*?)]/
   if(mentionMatch.find()){
     // if no id was found add the email address as text not as mention.
     def userId = nodeHelper.getUserByEmail(mentionMatch.group(1))?.key
     if (!userId) return [type:"Text",text:mentionMatch.group(1)]
-    return [type:"Mention",text:userId]  
+    return [type:"Mention",id:userId]  
   }
   return [type:"Text",text:type]    
 }
@@ -31,13 +34,13 @@ def createSegmentBody(String comment){
   for (int i = 0; i < splittedComment.size(); i++){
     if (splittedComment[i] == "") continue;
     if (i < splittedComment.size() - 1) {
-      segmentBody.add(setMessageSegment(splittedComment[i]))
+      segmentBody.add(getMessageSegment(splittedComment[i]))
     }else{
-      segmentBody.add(setMessageSegment(splittedComment[i]))
+      segmentBody.add(getMessageSegment(splittedComment[i]))
     }
   }
   // Return json object
-  return [subjectId: entity.key, body: [ messageSegments: segmentBody]]
+  return JsonOutput.toJson([subjectId: entity.key, body: [ messageSegments: segmentBody]])
 }
 
 def addComments(def comments){
@@ -45,8 +48,8 @@ def addComments(def comments){
   // collect all comments and post the createSegmentBody...
   comments.collect{ c ->
     if (c.body){
-      def res = httpClient.post("/services/data/v54.0/chatter/feed-elements/",JsonOutput.toJson(createSegmentBody(c.body)),null, ["Content-Type":["application/json"]]){
-        req ,
+      def res = httpClient.post("/services/data/v54.0/chatter/feed-elements/",createSegmentBody(c.body),null, ["Content-Type":["application/json"]]){
+        req,
         res ->
         // When the rest api call is successfully fulfilled set the SFCommentId var.
         if (res.code == 201) {
@@ -69,17 +72,21 @@ def addComments(def comments){
   }
 }
 
-
-if(firstSync){
-  entity.entityType = "Case"
-}
-
 if(entity.entityType == "Case"){
+  // Add the required fields you need to create a case in the firstSync.
+  if(firstSync){
+    entity.Subject      = replica.summary
+    entity.Origin       = "Web"
+    entity.Status       = "New"
+    // Store to create the Case ID so we can add the comment in the right case.
+    store(entity)
+  }
   entity.Subject      = replica.summary
   entity.Description  = replica.description
   entity.Origin       = "Web"
   entity.Status       = "New"
-  entity.comments     = commentHelper.mergeComments(entity, replica)
   entity.attachments  = attachmentHelper.mergeAttachments(entity, replica)
+  
+  // Set comments with or without user mentions
   addComments(replica.addedComments)
 }
--- Salesforce/Add User Mentions in Comments/outgoing.groovy
+++ Salesforce/Add User Mentions in Comments/outgoing.groovy
@@ -1,10 +1,29 @@
-// Change the mention id to the email
+// Change the mention id to the email 
+
+// Jira Cloud outgoing
 def changeString(String comment){
   def matcher = comment =~ /\[~accountid:(.*?)]/ 
   if (!matcher.find()) return comment
   
   matcher.each{
     comment = comment.replace(matcher.group(1), nodeHelper.getUser(matcher.group(1))?.email ?: "Stranger")
+  }
+  return comment
+}
+replica.comments = issue.comments.collect{
+  c ->
+  c.body = changeString(c.body)
+  c 
+}
+
+
+// Azure DevOps outgoing
+def changeString(String comment){
+  def matcher = comment =~ /<a href="#" data-vss-mention="version:[0-9.]+,([a-f0-9-]+)">[^<]+<\/a>/
+  if (!matcher.find()) return comment
+  matcher.each{
+  def userEmail = nodeHelper.getUser(matcher.group(1), workItem.projectKey)?.email ?: "johnDoe@tmp.com"
+  comment = comment.replace(matcher.group(0), "[~accountid:${userEmail}]")
   }
   return comment
 }
