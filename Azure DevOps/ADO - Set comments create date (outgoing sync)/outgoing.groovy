e5e56881fd45536ee406121ed313ea0eccffd419
christophe.debeule <christophe.debeule@idalko.com>
2025-05-16 06:53:57 -0700

    UPDATE: Body changes if wanted

--- Azure DevOps/ADO - Set comments create date (outgoing sync)/outgoing.groovy
+++ Azure DevOps/ADO - Set comments create date (outgoing sync)/outgoing.groovy
@@ -1,22 +1,31 @@
 import java.time.Instant
 import java.util.Date
 
-def tmp = httpClient.get("/${workItem.projectKey}/_apis/wit/workItems/652/comments?api-version=7.1-preview.4".toString(), true)
+// Fetch the comments
+def tmp = httpClient.get("/${workItem.projectKey}/_apis/wit/workItems/${workItem.key}/comments?api-version=7.1-preview.4".toString(), true)
 def enrichedComments = null
 if(tmp){
    tmp = tmp?.comments
+   // Collect all comments and check the ID to change the right comment where needed
    enrichedComments = workItem.comments.collect{ hubComment ->
       def matchingAzureComment = tmp.find { it.id == hubComment.id }
       if(matchingAzureComment){
-         hubComment.created = Date.from(Instant.parse(matchingAzureComment.createdDate))
+         // Here we can change the body, creation date, author etc with the values we get form the ADO comment.
+         Date date = Date.from(Instant.parse(matchingAzureComment.createdDate))
+         hubComment.body = "[Created on: ${date}]\n\n${hubComment.body}"
+         hubComment.created = date
       }
+      // Return the full Exalate comment object
       return hubComment
    }
 }
+// If there are no enriched comments just return the default setup.
 if(enrichedComments){
-    replica.comments = enrichedComments
+   replica.comments = enrichedComments
 }else{
-    replica.comments = workItem.comments
+   replica.comments = workItem.comments
 }
+
+workItem.comments = enrichedComments
 
 throw new Exception("${replica.comments}")
